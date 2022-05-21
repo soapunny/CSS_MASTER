dab6fa812dd6966ea74225e5c7d2ff970485fadc
의섭 소 <soapunny@gmail.com>
2022-05-21 23:29:34 +0900

    node js server and css updated

--- Grid/server.js
+++ Grid/server.js
@@ -5,9 +5,9 @@ const url = require('url');
 const fs = require('fs');
 
 http.createServer((request, response) => {
-    const path = url.parse(request.url, true).pathname; // url���� path ����
-    if (request.method === 'GET') { // GET ��û�̸�
-        if (path === '/') { // �ּҰ� /�̸�
+    const path = url.parse(request.url, true).pathname; 
+    if (request.method === 'GET') { 
+        if (path === '/') { 
             response.writeHead(200, { 'Content-Type': 'text/html' });
             fs.readFile(__dirname + '/html/main.html', (err, data) => {
                 if (err) {
@@ -15,15 +15,29 @@ http.createServer((request, response) => {
                 }
                 response.end(data, 'utf-8');
             });
-        } else if (path === '/group') { // �ּҰ� /about�̸�
-            response.writeHead(200, { 'Content-Type': 'text/html' }); // header ����
-            fs.readFile(__dirname + '/html/grid_template.html', (err, data) => { // ���� �д� �޼ҵ�
+        } else if (path === '/css/main.css') {
+            fs.readFile(__dirname + '/css/main.css', (err, data) => { 
                 if (err) {
-                    return console.error(err); // ���� �߻��� ���� ����ϰ� ����
+                    return console.error(err);
                 }
-                response.end(data, 'utf-8'); // �������� ����
+                response.end(data, 'utf-8'); 
             });
-        } else if (path === '/auto') { // �ּҰ� /�̸�
+        } else if (path === '/group') { 
+            response.writeHead(200, { 'Content-Type': 'text/html' }); 
+            fs.readFile(__dirname + '/html/grid_template.html', (err, data) => { 
+                if (err) {
+                    return console.error(err); 
+                }
+                response.end(data, 'utf-8'); 
+            });
+        } else if (path === '/css/grid_template.css') {
+            fs.readFile(__dirname + '/css/grid_template.css', (err, data) => { 
+                if (err) {
+                    return console.error(err);
+                }
+                response.end(data, 'utf-8'); 
+            });
+        }else if (path === '/auto') {
             response.writeHead(200, { 'Content-Type': 'text/html' });
             fs.readFile(__dirname + '/html/auto.html', (err, data) => {
                 if (err) {
@@ -31,7 +45,14 @@ http.createServer((request, response) => {
                 }
                 response.end(data, 'utf-8');
             });
-        } else if (path === '/line') { // �ּҰ� /�̸�
+        } else if (path === '/css/auto.css') {
+            fs.readFile(__dirname + '/css/auto.css', (err, data) => { 
+                if (err) {
+                    return console.error(err);
+                }
+                response.end(data, 'utf-8'); 
+            });
+        } else if (path === '/line') { 
             response.writeHead(200, { 'Content-Type': 'text/html' });
             fs.readFile(__dirname + '/html/line_naming.html', (err, data) => {
                 if (err) {
@@ -39,7 +60,14 @@ http.createServer((request, response) => {
                 }
                 response.end(data, 'utf-8');
             });
-        } else if (path === '/fraction') { // �ּҰ� /�̸�
+        } else if (path === '/css/line_naming.css') {
+            fs.readFile(__dirname + '/css/line_naming.css', (err, data) => { 
+                if (err) {
+                    return console.error(err);
+                }
+                response.end(data, 'utf-8'); 
+            });
+        } else if (path === '/fraction') { 
             response.writeHead(200, { 'Content-Type': 'text/html' });
             fs.readFile(__dirname + '/html/fraction.html', (err, data) => {
                 if (err) {
@@ -47,7 +75,14 @@ http.createServer((request, response) => {
                 }
                 response.end(data, 'utf-8');
             });
-        } else if (path === '/items') { // �ּҰ� /�̸�
+        } else if (path === '/css/fraction.css') {
+            fs.readFile(__dirname + '/css/fraction.css', (err, data) => { 
+                if (err) {
+                    return console.error(err);
+                }
+                response.end(data, 'utf-8'); 
+            });
+        } else if (path === '/items') { 
             response.writeHead(200, { 'Content-Type': 'text/html' });
             fs.readFile(__dirname + '/html/items.html', (err, data) => {
                 if (err) {
@@ -55,7 +90,14 @@ http.createServer((request, response) => {
                 }
                 response.end(data, 'utf-8');
             });
-        } else if (path === '/autoColumns') { // �ּҰ� /�̸�
+        } else if (path === '/css/items.css') {
+            fs.readFile(__dirname + '/css/items.css', (err, data) => { 
+                if (err) {
+                    return console.error(err);
+                }
+                response.end(data, 'utf-8'); 
+            });
+        } else if (path === '/autoColumns') { 
             response.writeHead(200, { 'Content-Type': 'text/html' });
             fs.readFile(__dirname + '/html/autoColumns.html', (err, data) => {
                 if (err) {
@@ -63,7 +105,14 @@ http.createServer((request, response) => {
                 }
                 response.end(data, 'utf-8');
             });
-        } else if (path === '/minMax') { // �ּҰ� /�̸�
+        } else if (path === '/css/autoColumns.css') {
+            fs.readFile(__dirname + '/css/autoColumns.css', (err, data) => { 
+                if (err) {
+                    return console.error(err);
+                }
+                response.end(data, 'utf-8'); 
+            });
+        } else if (path === '/minMax') { 
             response.writeHead(200, { 'Content-Type': 'text/html' });
             fs.readFile(__dirname + '/html/minMax.html', (err, data) => {
                 if (err) {
@@ -71,7 +120,14 @@ http.createServer((request, response) => {
                 }
                 response.end(data, 'utf-8');
             });
-        } else if (path === '/minMaxContent') { // �ּҰ� /�̸�
+        } else if (path === '/css/minMax.css') {
+            fs.readFile(__dirname + '/css/minMax.css', (err, data) => { 
+                if (err) {
+                    return console.error(err);
+                }
+                response.end(data, 'utf-8'); 
+            });
+        } else if (path === '/minMaxContent') { 
             response.writeHead(200, { 'Content-Type': 'text/html' });
             fs.readFile(__dirname + '/html/minMaxContent.html', (err, data) => {
                 if (err) {
@@ -79,9 +135,16 @@ http.createServer((request, response) => {
                 }
                 response.end(data, 'utf-8');
             });
-        } else { // ��Ī�Ǵ� �ּҰ� ������
-            response.statusCode = 404; // 404 ���� �ڵ�
-            response.end('�ּҰ� �����ϴ�');
+        } else if (path === '/css/minMaxContent.css') {
+            fs.readFile(__dirname + '/css/minMaxContent.css', (err, data) => { 
+                if (err) {
+                    return console.error(err);
+                }
+                response.end(data, 'utf-8'); 
+            });
+        } else {
+            response.statusCode = 404;
+            response.end('Server Error');
         }
     }
 }).listen(11080);
